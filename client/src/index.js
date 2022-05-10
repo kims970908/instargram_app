5d43ca88b8474316d4aae5acdc32cd9c089c17cc
kims970908 <kmis970908@gmail.com>
2022-05-10 16:57:05 +0900

    userCrtl에서 getuser생성 userprofile 검색 가능

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import DataProvider from "./redux/store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <DataProvider>
+    <App />
+  </DataProvider>
 );
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
